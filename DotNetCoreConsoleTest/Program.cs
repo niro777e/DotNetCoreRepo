a947d5cab849151c47ac22b34fec1f4e53e7d64b
Nir Einat <niro757@outlook.com>
2018-12-21 00:10:09 +0200

    adding new project to git

---
+++ DotNetCoreConsoleTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NetCoreLibraryTest;
+
+namespace DotNetCoreConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NetCoreLibraryTest.Utilities.PrintHelloWorld());
+
+            Console.ReadKey();
+        }
+    }
+}
